973d8d42ff6bfaedcdaf2bd256e7732f684e5fb8
Erwan Mahe <er1mahe@gmail.com>
2026-08-29 11:07:34 +0200

    Initial Commit

---
+++ repas/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  repas
+//
+//  Created by erwan mahe on 29/08/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ repas/repasApp.swift
@@ -0,0 +1,17 @@
+//
+//  repasApp.swift
+//  repas
+//
+//  Created by erwan mahe on 29/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct repasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
